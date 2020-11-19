935e6f1a36aab741d720218846e8d5d6e20a8549
Allen-TaiLin <skygary2000@gmail.com>
2020-11-19 19:19:51 +0800

    feat: add handlebars

--- app.js
+++ app.js
@@ -1,12 +1,28 @@
+// require packages used in the projects
 const express = require('express')
+const exphbs = require('express-handlebars')
+const bodyParser = require('body-parser')
 const app = express()
 const port = 3000
 
+// setting template engine
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
+// 用 app.use 規定每一筆請求都需要透過 body-parser 進行前置處理
+app.use(bodyParser.urlencoded({ extended: true }))
+// setting static files
+app.use(express.static('public'))
 
+// 設定資料庫
+require('./config/mongoose')
+
+
+// routes setting
 app.get('/', (req, res) => {
   res.send('Account Login')
 })
 
+// start and listen on the Express server
 app.listen(port, () => {
   console.log(`Express is listening on http://localhost:${port}`)
 })
