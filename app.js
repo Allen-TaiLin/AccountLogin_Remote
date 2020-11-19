86212952ac972fe277cce9bffee06f1e6957d374
Allen-TaiLin <skygary2000@gmail.com>
2020-11-20 01:27:44 +0800

    feat: add views

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const bodyParser = require('body-parser')
 const app = express()
 const port = 3000
 
+const AccountData = require('./models/account')
+
 // setting template engine
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
@@ -19,7 +21,42 @@ require('./config/mongoose')
 
 // routes setting
 app.get('/', (req, res) => {
-  res.send('Account Login')
+  // 讀取index檔案、渲染畫面
+  return res.render('index')
+})
+
+app.post('/login', (req, res) => {
+  // 從 req.body 拿出表單裡的資料
+  const options = req.body
+
+  // 取出 AccountData model 裡的所有資料
+  AccountData.find()
+    .lean()
+    .then((accounts) => {
+      // 比對帳密
+      const user = accounts.find((item) => {
+        return (item.email === options.email) && (item.password === options.password)
+      })
+
+      // 帳密吻合
+      if (user !== undefined) {
+        return res.render('show', { user, message: '' })
+      } else {
+        // 帳密不符，檢查是否有帳號
+        const mistake = accounts.find((item) => {
+          return (item.email === options.email) && !(item.password === options.password)
+        })
+
+        if (mistake !== undefined) {
+          return res.render('index', { user: options, message: '密碼錯誤!' })
+        } else {
+          return res.render('index', { user: options, message: '無此帳號!' })
+        }
+      }
+
+    })
+    .catch((error) => console.log(error))  // 錯誤處理
+
 })
 
 // start and listen on the Express server
